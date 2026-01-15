e93d0b670d0ab14339904f53adbdb5ae1f6a68ea
peekitn <franciscoafbatalha@hotmail.com>
2026-01-15 18:22:44 -0300

    commitando js

---
+++ JAVASCRIPT/ex.js
@@ -0,0 +1,97 @@
+```
+var dogAge = prompt("How old is your dog?");
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog is" + humanAge + "years-old in human years");
+
+var message = "fuck u"
+var name = "six seven"
+alert(message + "jackass," + "" + name);
+
+function getMilk(){ 
+    console.log("leave house");
+    console.log("scream for me");
+}
+getMilk();
+
+alert("Hello");
+alert("World");
+window.alert("HelloWorld");
+var myName = "Alexandre";
+alert(myName);
+var yourName = prompt("What is your name?");
+alert("My name is" + ", welcome to my course " + yourName + "!");
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+alert("Your level is currently: " + gameLevel);
+
+function getMilk(bootles) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("buy " + bootles + " bootles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(12);
+
+var tweet = prompt("Compose your tweet:");
+var tweetCount = tweet.length;
+alert("You have writeen" + tweetCount + "characters, you have" + (140 - tweetCount) + "characters remaining.");
+
+var myName = "Hoopa";
+var yggdrasil = "SixSeven";
+
+var name = "Alexandre";
+name.toUpperCase();
+name = name.toLowerCase();
+
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+
+    var c = a;
+    a = b;
+    b = c;
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+var n = Math.random();
+n = n * 6
+n = Math.floor(n) + 1;
+console.log(n);
+
+prompt("What is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1; //1-100
+
+if (loveScore > 70) {
+    alert("Your love score is" + loveScore + "%" + "You love each oher like Kanye loves Kanye");
+}else{
+    alert("Your love score is" + loveScore + "%");
+}
+
+
+```
